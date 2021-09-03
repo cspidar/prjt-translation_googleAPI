f9d378d8a2a4f8754b19a062500d4345525f88fc
Cspidar <cspidar@gmail.com>
2021-09-03 15:41:56 +0900

    2

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ async function translateTextWithGlossary() {
     for (const translation of response.glossaryTranslations) {
       fs.writeFileSync(
         `${dirName}/${baseName}_en${extName}`,
-        "\ufeff" + translation.translatedText,
+        translation.translatedText,
         {
           encoding: "utf8",
         }
@@ -83,13 +83,9 @@ async function translateTextWithGlossary() {
       "utf-8"
     );
     editText = afterTranslateTextEdit(editText);
-    fs.writeFileSync(
-      `${dirName}/${baseName}_en${extName}`,
-      "\ufeff" + editText,
-      {
-        encoding: "utf8",
-      }
-    );
+    fs.writeFileSync(`${dirName}/${baseName}_en${extName}`, editText, {
+      encoding: "utf8",
+    });
   }
   editSecond();
 
@@ -103,7 +99,28 @@ function afterTranslateTextEdit(beforeEditText) {
   let afterEditText;
   afterEditText = beforeEditText
     .replaceAll(":imagesdir:doc", ":imagesdir: doc")
-    .replaceAll(":icon_dir:image", ":icon_dir: image");
+    .replaceAll(":icon_dir:image", ":icon_dir: image")
+    .replaceAll(
+      "include::doc\\000_preface\\preface.adoc[]",
+      "include::doc\\000_preface\\preface_en.adoc[]"
+    )
+    .replaceAll("doc.adoc[leveloffset=+1]", "doc_en.adoc[leveloffset=+1]")
+    .replaceAll(
+      ":title-page-background-image: image:./images/covers/title-bg_A5.png[]",
+      "// :title-page-background-image: image:./images/covers/title-bg_A5.png[]"
+    )
+    .replaceAll(
+      ":back-cover-image: image:./images/covers/back-cover_A5.pdf[]",
+      "// :back-cover-image: image:./images/covers/back-cover_A5.pdf[]"
+    )
+    .replaceAll(
+      "// :title-page-background-image: image:./images/covers/title-bgEN_A5.png[]",
+      ":title-page-background-image: image:./images/covers/title-bgEN_A5.png[]"
+    )
+    .replaceAll(
+      "// :back-cover-image: image:./images/covers/back-coverEN_A5.pdf[]",
+      ":back-cover-image: image:./images/covers/back-coverEN_A5.pdf[]"
+    );
 
   return afterEditText;
 }
