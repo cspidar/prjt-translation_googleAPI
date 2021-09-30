28507d229ede7feb201b502d73c20c3a91d9b8ea
Cspidar <cspidar@gmail.com>
2021-09-30 14:34:52 +0900

    1

--- src/func/afterTranslateTextEdit.js
+++ src/func/afterTranslateTextEdit.js
@@ -1,21 +1,15 @@
 // 번역된 텍스트 후보정
 export function afterTranslateTextEdit(beforeEditText) {
   let afterEditText;
-  // const headingSentence = new RegExp("^==.*");
-  const headingLineNoBracket = /(?<==+.*(?<=\s))\w*/g;
-  const headingLetterAfterSpace = /(?<==+.*(?<=\s))[a-z]/g;
 
   // const plusSpaceAfterLetter = /(?<=\n\+ )[A-Za-z]/g;
-  const plusSpaceLetter = /(?<=\n\+) (?=[A-Za-z])/g;
 
-  const titleSpaceLetter = /(?<=\n\.) (?=[A-Za-z].*\n(\[|\*|image::))/g;
-
-  const firstLetter =
-    /^[a-z]|(?<=\n)[a-z]|(?<=\n\.\s)[a-z]|(?<=\n\*\s)[a-z]|(?<=\n\-\s)[a-z]|(?<=\|\s)[a-z]|(?<=\|)[a-z]/g;
-
+  const headingLineNoBracket = /(?<==+.*(?<=\s))\w*/g;
   beforeEditText = beforeEditText.replace(headingLineNoBracket, (p) =>
     p.toLowerCase()
   );
+
+  const headingLetterAfterSpace = /(?<==+.*(?<=\s))[a-z]/g;
   beforeEditText = beforeEditText.replace(headingLetterAfterSpace, (p) =>
     p.toUpperCase()
   );
@@ -24,20 +18,24 @@ export function afterTranslateTextEdit(beforeEditText) {
 
   // 헤딩 단어 첫글자 대문자화에서 전치사 제외
   beforeEditText = beforeEditText
-    .replaceAll(/ On /g, " on ")
-    .replaceAll(/ And /g, " and ")
-    .replaceAll(/ Or /g, " or ")
-    .replaceAll(/ Of /g, " of ")
-    .replaceAll(/ For /g, " for ")
-    .replaceAll(/ Through /g, " through ");
+    .replaceAll(/. On /g, " on ")
+    .replaceAll(/. And /g, " and ")
+    .replaceAll(/. Or /g, " or ")
+    .replaceAll(/. Of /g, " of ")
+    .replaceAll(/. For /g, " for ")
+    .replaceAll(/. Through /g, " through ");
 
   // title이 ordered list 로 변경되는 것 복구 (용어집 사용 단어가 타이틀이 될 경우 발생)
+  const titleSpaceLetter = /(?<=\n\.) (?=[A-Za-z].*\n(\[|\*|image::))/g;
   beforeEditText = beforeEditText.replace(titleSpaceLetter, "");
 
   // 플러스 뒤 공백 -> 줄바꿈
+  const plusSpaceLetter = /(?<=\n\+) (?=[A-Za-z])/g;
   beforeEditText = beforeEditText.replace(plusSpaceLetter, "\n");
 
   // 문장 첫글자 대문자화
+  const firstLetter =
+    /^[a-z]|(?<=\n)[a-z]|(?<=\n\.\s)[a-z]|(?<=\n\*\s)[a-z]|(?<=\n\-\s)[a-z]|(?<=\|\s)[a-z]|(?<=\|)[a-z]/g;
   beforeEditText = beforeEditText.replace(firstLetter, (p) => p.toUpperCase());
 
   beforeEditText = beforeEditText.replaceAll("‘", "`").replaceAll("’", "`");
